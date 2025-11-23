6c9522dfc2bd26c5a68ee2f005033cf3fcaf8a76
gong <sattelasubaru@gmail.com>
2025-11-24 01:18:15 +0800

    'asd'

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    base: '/j4/public/', // include /public/ since your site uses it
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
